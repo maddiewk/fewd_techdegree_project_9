3422848c9d7459896abbb5e5af3e183258e23f59
Madeline Watson <mcwatson91@gmail.com>
2019-05-21 14:06:56 -0400

    more about

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-
+window.onscroll = function() {
+    const nav = document.getElementById('nav');
+    if ( window.pageYOffset > 100 ) {
+      // set some sort of function to fade? Try something else?
+        nav.classList.add("navbar1");
+    } else {
+        nav.classList.remove("navbar1");
+    }
+}
