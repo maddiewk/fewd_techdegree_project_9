6db6487c1ece0a6f0760d7da85569b6742e1e2e7
Madeline Watson <mcwatson91@gmail.com>
2019-06-20 12:50:06 -0400

    projects js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
-  $('#fade').fadeIn(1800);
+  // $('#fade').fadeIn(1800);
   $('#fade2').fadeIn(6000);
+  // $('#fade').animate({left: '150px'}, 500);
+  $('#fade').animate({'margin-right': 0}, 'slow');
+
 });
 
 function openNav() {
@@ -19,8 +22,9 @@ $('.closebtn').on('click', function() {
   closeNav();
 });
 
-// load separate content without reloading the entire page
-// const contentWrapper = document.querySelector('.testport');
-// $('.testabout').on('click', function() {
-//   contentWrapper.innerHTML = about;
-// });
+// load separate content without reloading the entire page?
+
+const contentWrapper = document.querySelector('.testport');
+$('.testabout').on('click', function() {
+  contentWrapper.innerHTML = about;
+});
