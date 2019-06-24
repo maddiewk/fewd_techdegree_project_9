4d3470610c353b23d4d6fa703410ac3a1bc71047
Madeline Watson <mcwatson91@gmail.com>
2019-06-24 14:09:36 -0400

    consolidate sass

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $('.fadePage').fadeIn(1800);
-  $('#fade2').fadeIn(4000); 
+  $('#fade2').fadeIn(4000);
 });
 
 function openNav() {
@@ -32,13 +32,12 @@ $('.input-submit').on('click', function(e) {
   $appendDiv.css('display', 'block');
   $appendDiv.html('');
   if ($name.val() === '' || $email.val() === '' || $message.val() === '') {
-    $appendDiv.delay(3000).append(`<span class="alert-message">Please fill in required fields.</span>`).slideDown(1500).removeClass('alert-hidden').hide(1000);
+    $appendDiv.append(`<span class="alert-message">Please fill in required fields.</span>`).slideDown(1500).hide(1000);
   } else {
-    $appendDiv.delay(3000).append(`<span class="success-message">Your message has been sent.</span>`).slideDown(1500).removeClass('alert-hidden').hide(1000);
+    $appendDiv.append(`<span class="success-message">Your message has been sent.</span>`).slideDown(1500).hide(1000);
     $name.val('');
     $email.val('');
     $phone.val('');
     $message.val('');
   }
-
 });
